289b778358d9e7a6a529e215d333d5d8150701e4
ManuelCanul109 <manuel.canul109@gmail.com>
2020-11-12 16:02:04 -0500

    Cambios Finales de Manuel

--- app/src/main/java/com/example/tarea_8_aguero_castillo_canul_serrano/gridActivity.kt
+++ app/src/main/java/com/example/tarea_8_aguero_castillo_canul_serrano/gridActivity.kt
@@ -1,19 +1,23 @@
 package com.example.tarea_8_aguero_castillo_canul_serrano
 
-import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.tarea_8_aguero_castillo_canul_serrano.adapters.AlphaAdapters
 import com.basic.programming.mygridlayoutapp.model.CharItem
+import com.example.tarea_8_aguero_castillo_canul_serrano.adapters.AlphaAdapters
+import kotlin.random.Random
+
 
 class gridActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var charItem: ArrayList<CharItem>? = null
     private var gridLayoutManager: GridLayoutManager? = null
     private var alphaAdapters: AlphaAdapters? = null
+    private var PAQUETE_APP = "com.example.tarea_8_aguero_castillo_canul_serrano"
+    private var PAQUETE_SRC = "drawable"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,19 +38,26 @@ class gridActivity : AppCompatActivity() {
 
     private fun setAlphas(): ArrayList<CharItem> {
 
+        val start = 1
+        val end = 26
+
         var arrayList: ArrayList<CharItem> = ArrayList()
 
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
-        arrayList.add(CharItem(R.drawable.trompeta, "trompeta"))
-        arrayList.add(CharItem(R.drawable.trompeta, "trompeta"))
-        arrayList.add(CharItem(R.drawable.trompeta, "trompeta"))
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
-        arrayList.add(CharItem(R.drawable.violin, "violin"))
+        for (i in 1..12) {
 
+            var vario = rand(start, end)
 
+            val name = "letter_$vario"
+
+            val resource = resources.getIdentifier(name, PAQUETE_SRC,PAQUETE_APP)
+
+            arrayList.add(CharItem(resource, ""+vario))
+        }
         return arrayList
+    }
+
+    fun rand(start: Int, end: Int): Int {
+        require(!(start > end || end - start + 1 > Int.MAX_VALUE)) { "Illegal Argument" }
+        return Random(System.nanoTime()).nextInt(end - start + 1) + start
     }
 }
